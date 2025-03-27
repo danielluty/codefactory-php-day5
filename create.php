4dd4de76a24d6505be9faa488d7c7c08980c8688
Daniel Luty <lutydaniel@gmail.com>
2025-03-27 15:51:10 +0100

    added create page

---
+++ create.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Create product</h1>
+    <hr>
+
+    <form action="" method="POST">
+        <label for="">
+            Product name:
+            <input type="text" name="" id="">
+        </label>
+        <br>
+        <label for="">
+            Product price:
+            <input type="number" name="" id="" step="0.01">
+        </label>
+        <br>
+        <label for="">
+            Product image:
+            <input type="text" name="" id="">
+        </label>
+        <hr>
+        <button type="submit">Create</button>
+    </form>
+</body>
+
+</html>
